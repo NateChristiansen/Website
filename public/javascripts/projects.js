b86e4617b388962b9df3c40699b379e1ba3b6275
Nate Christiansen <natechristiansen42@gmail.com>
2016-08-13 20:19:46 -0400

    Added project modal that is updated by angular

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -3,4 +3,15 @@ angular.module('projects', []).controller('ProjectsController', ['$scope', funct
 		$scope.projects = data;
 		$scope.$apply();
 	});
+
+	$scope.setcurrent = function(project) {
+		$scope.currentProject = project;
+		$('#ProjectModal').css('display', 'block');
+	};
 }]);
+
+$(document).ready(function() {
+	$('#ProjectModal > img').click(function() {
+		$('#ProjectModal').css('display', 'none');
+	});
+});
